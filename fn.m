2ea9f9de87529a7c6dc5b56c28be7efa726bf8f0
wyattcheang <wyattcheangwaihoe@icloud.com>
2024-03-23 23:08:48 +0800

    feat: rename file, use grid layout

--- fn.m
+++ fn.m
@@ -1,9 +1,16 @@
 classdef fn
     methods (Static)
         function img = img2gray(inputImg)
-            % Example processing: convert image to grayscale
-            img = rgb2gray(inputImg);
-            img = cat(3, img, img, img);
+             % Check if the input image is already grayscale
+            if size(inputImg, 3) == 1
+                % Input image is already grayscale, return it directly
+                img = inputImg;
+            else
+                % Convert the image to grayscale
+                img = rgb2gray(inputImg);
+                % Replicate the grayscale channel to form an RGB image
+                img = cat(3, img, img, img);
+            end
         end
     end
 end
